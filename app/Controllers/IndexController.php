a565723154b07b2599e5c3f926405a5f2865716e
ozne32 <84781872+ozne32@users.noreply.github.com>
2024-06-21 21:39:33 -0300

    Update IndexController.php

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -71,17 +71,7 @@ class IndexController extends Action
         }
 
         try {
-            $mail = new PHPMailer(true);//precisa gerar um objeto, pq se não daonde ia estar saindo tudo dps ?
-            //Server settings -> email que você vai usar como servidor
-            $mail->SMTPDebug = false;                      //Enable verbose debug output
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
-            $mail->SMTPAuth = true;                                   //Enable SMTP authentication
-            $mail->Username = 'enzorcc.mf3rs@gmail.com';                     //SMTP username
-            $mail->Password = 'senha legal 123454285043';                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
-            $mail->Port = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-
+           
             //Recipients -> remetente
             $mail->setFrom($mensagem->__get('destino'), $mensagem->__get('nome'));
             $mail->addAddress($mensagem->__get('destino'));     //Add a recipient
