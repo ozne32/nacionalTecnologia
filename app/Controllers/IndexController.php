e219e64d5e1be188579a45e7fb305978220cb40a
ozne32 <enzorcc.mf3rs@gmail.com>
2024-06-24 20:02:02 -0300

    atualizando a parte do email

--- app/Controllers/IndexController.php
+++ app/Controllers/IndexController.php
@@ -2,11 +2,11 @@
 
 namespace App\Controllers;
 
-require  __DIR__ ."/../PHPMailer/Exception.php";
-require  __DIR__ ."/../PHPMailer/SMTP.php";
-require  __DIR__ ."/../PHPMailer/POP3.php";
-require  __DIR__ ."/../PHPMailer/PHPMailer.php";
-require __DIR__ ."/../PHPMailer/OAuth.php";
+require __DIR__ . "/../PHPMailer/Exception.php";
+require __DIR__ . "/../PHPMailer/SMTP.php";
+require __DIR__ . "/../PHPMailer/POP3.php";
+require __DIR__ . "/../PHPMailer/PHPMailer.php";
+require __DIR__ . "/../PHPMailer/OAuth.php";
 use MF\Controller\Action;
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
@@ -31,8 +31,10 @@ class Mensagem
     }
     public function validaMensagem()
     {
-        if (empty($this->email) || empty($this->assunto) || 
-        empty($this->empresa) || empty($this->telefone)|| empty($this->pergunta)) {
+        if (
+            empty($this->email) || empty($this->assunto) ||
+            empty($this->empresa) || empty($this->telefone) || empty($this->pergunta)
+        ) {
             return true; //se tudo estiver vazio retorne falso
         } else {
             return false;
@@ -75,46 +77,56 @@ class IndexController extends Action
             // echo '<pre>';
             // print_r($mensagem);
             // echo '</pre>';
-        }else{
-            try {
-                $mail = new PHPMailer(true);//precisa gerar um objeto, pq se não daonde ia estar saindo tudo dps ?
-                //Server settings -> email que você vai usar como servidor
-                                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
-                // echo '<pre>';
-                // print_r($mail);
-                // echo '</pre>';            
-    
-                //Recipients -> remetente
-                $mail->setFrom('enzorcc.mf3rs@gmail.com', 'enzo'); //remetente
-                $mail->addAddress('enzorcc.mf3rs@gmail.com');
-                // $mail->addAddress($mensagem->__get('email'), 'Seu email já foi encaminhado para a assistência tecnica');     //destinatário
-                // $mail->addReplyTo('info@example.com', 'Information'); isso é o email que vai receber a resposta do email acima
-                //$mail->addCC('cc@example.com');
-                // $mail->addBCC('bcc@example.com');
-    
-                //Attachments
-                // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-                // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-    
-                //Content
-                $mail->isHTML(true);                                  //Set email format to HTML
-                $mail->Subject = $mensagem->__get('assunto');
-                $mail->Body = $mensagem->__get('mensagem');
-                $mail->AltBody = strip_tags($mensagem->__get('mensagem'));
-                $mail->send();
-                // $mail->AltBody = "Nome:$mensagem->nome  Email:$mensagem->email Empresa: $mensagem->empresa 
-                // Pergunta: $mensagem->pergunta Telefone: $mensagem->telefone";
-                // $mail->send();
-                $mensagem->__set("status['codigo_status']", 1);
-                $mensagem->__set("status['descricao_status']", 'sua mensagem foi enviada');
-                
-                header('location: /contactus?mensagem=enviada');
-            } catch (Exception $e) {
-                $mensagem->__set("status['codigo_status']", 2);
-                $mensagem->__set("status['descricao_status']", 'Sua mensagem não foi mandada. Detalhes sobre o erro ' . $mail->ErrorInfo);
+        } else {
+            if (filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+                try {
+                    $mail = new PHPMailer(true);//precisa gerar um objeto, pq se não daonde ia estar saindo tudo dps ?
+                    //Server settings -> email que você vai usar como servidor
+                    $mail->SMTPDebug = false;                      //Enable verbose debug output
+                    $mail->isSMTP();                                            //Send using SMTP
+                    $mail->Host = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                    $mail->SMTPAuth = true;                                   //Enable SMTP authentication
+                    $mail->Username = 'email.com';                     //SMTP username
+                    $mail->Password = 'senha1234';                               //SMTP password
+                    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
+                    $mail->Port = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
+                    // echo '<pre>';
+                    // print_r($mail);
+                    // echo '</pre>';            
+
+                    //Recipients -> remetente
+                    $mail->setFrom('enzorcc.mf3rs@gmail.com', 'enzo'); //remetente
+                    $mail->addAddress('enzorcc.mf3rs@gmail.com');
+                    // $mail->addAddress($mensagem->__get('email'), 'Seu email já foi encaminhado para a assistência tecnica');     //destinatário
+                    // $mail->addReplyTo('info@example.com', 'Information'); isso é o email que vai receber a resposta do email acima
+                    //$mail->addCC('cc@example.com');
+                    // $mail->addBCC('bcc@example.com');
+
+                    //Attachments
+                    // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+                    // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+
+                    //Content
+                    $mail->isHTML(true);                                  //Set email format to HTML
+                    $mail->Subject = $mensagem->__get('assunto');
+                    $mail->Body = $mensagem->__get('mensagem');
+                    $mail->AltBody = strip_tags($mensagem->__get('mensagem'));
+                    $mail->send();
+                    // $mail->AltBody = "Nome:$mensagem->nome  Email:$mensagem->email Empresa: $mensagem->empresa 
+                    // Pergunta: $mensagem->pergunta Telefone: $mensagem->telefone";
+                    // $mail->send();
+                    $mensagem->__set("status['codigo_status']", 1);
+                    $mensagem->__set("status['descricao_status']", 'sua mensagem foi enviada');
+
+                    header('location: /contactus?mensagem=enviada');
+                } catch (Exception $e) {
+                    $mensagem->__set("status['codigo_status']", 2);
+                    $mensagem->__set("status['descricao_status']", 'Sua mensagem não foi mandada. Detalhes sobre o erro ' . $mail->ErrorInfo);
+                }
+            }else{
+                header('location:/contactus?preenchimento=erroEmail');
             }
         }
-        
     }
 }
 //importacao das bibliotecas
