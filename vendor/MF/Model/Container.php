30c4513fe411089828fff52d9e0ed7a9267b49de
ozne32 <enzorcc.mf3rs@gmail.com>
2024-06-26 17:04:54 -0300

    update correção palavra container

--- vendor/MF/Model/Container.php
+++ vendor/MF/Model/Container.php
@@ -7,7 +7,7 @@ use App\Connection;
 class Container {
 
 	public static function getModel($model) {
-		$class = "\\App\\Models\\".ucfirst($model);
+		$class = "\\App\\Model\\".ucfirst($model);
 		$conn = Connection::getDb();
 
 		return new $class($conn);
