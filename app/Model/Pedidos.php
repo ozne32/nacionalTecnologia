12f5808c6273403dfe56fcaafae4ff4c874d564f
ozne32 <enzorcc.mf3rs@gmail.com>
2024-06-26 17:04:05 -0300

    update métodos banco de dados

---
+++ app/Model/Pedidos.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Model;
+
+use MF\Model\Model;
+
+class Pedidos extends Model {
+	private $id;
+	private $nome;
+	private $telefone;
+	private $solucionado;
+	private $email;
+    private $empresa;
+    private $assunto;
+    private $pergunta;
+    public function __get($attr){
+        return $this->$attr;
+    }
+    public function __set($attr, $valor){
+        $this->$attr = $valor;
+        return $this;
+    }
+    public function adiciona(){
+        $query = "INSERT INTO tb_pedidos(nome,email,telefone,empresa,pergunta,assunto,solucionado)
+        values(:nome,:email,:telefone,:empresa,:pergunta,:assunto,0)";
+          echo '<pre>';
+          print_r($this);
+          echo '</pre>';
+        $smtm = $this->db->prepare($query);
+        $smtm->bindValue(':nome', $this->nome);
+        $smtm->bindValue(':email', $this->email);
+        $smtm->bindValue(':telefone', $this->telefone);
+        $smtm->bindValue(':empresa', $this->empresa);
+        $smtm->bindValue(':assunto', $this->__get('assunto'));
+        $smtm->bindValue(':pergunta', $this->pergunta);
+        return $smtm->execute();
+    }
+
+}
