63566745a6e145ceb00f7d62899a236bfd6339d9
ozne32 <enzorcc.mf3rs@gmail.com>
2024-06-26 17:03:04 -0300

    update conexão para o banco de dados

--- app/Connection.php
+++ app/Connection.php
@@ -8,7 +8,7 @@ class Connection {
 		try {
 
 			$conn = new \PDO(
-				"mysql:host=localhost;dbname=;charset=utf8",
+				"mysql:host=localhost;dbname=nacionaltecnologia;charset=utf8",
 				"root",
 				"" 
 			);
